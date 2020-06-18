4cf657bfa768968b1884867b23c030c005220f2e
DiptoChakrabarty <diptochuck123@gmail.com>
2020-06-18 16:28:34 +0530

    spec and container

--- routes.py
+++ routes.py
@@ -6,8 +6,11 @@ app=Flask(__name__)
 @app.route("/deployment",methods=["POST"])
 def deployment():
     data = request.get_json()
-
-
+    metadata_name = data["metadata_name"]
+    replicas = data["replicas"]
+    container_name = data["container_name"]
+    container_image =  data["container_image"]
+    container_port = data["container_port"]
 
     if os.getenv('KUBERNETES_SERVICE_HOST'):
         config.load_incluster_config()
@@ -21,10 +24,19 @@ def deployment():
     deploy.kind = "Deployment"
     deploy.metdata = client.V1ObjectMeta(name=metadata_name)
 
-
+    #define spec
     spec = client.V1DeploymentSpec()
     spec.replicas = replicas 
 
+    #define container
+    container = client.V1Container()
+    container.name=  container_name
+    container.image= container_image
+    container. ports = [client.V1ContainerPort(container_port=container_port)]
+
+    #create deployment
+    spec.template.spec.containers = [container]
+    deployment.spec = spec
 
 
 
