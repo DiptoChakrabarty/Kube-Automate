b14d067850c8cdbba80004a7432b933af7fe5dc5
DiptoChakrabarty <diptochuck123@gmail.com>
2020-06-18 16:14:07 +0530

    app file

--- routes.py
+++ routes.py
@@ -1,7 +1,31 @@
-from flask import Flask 
+from flask import Flask,request
 from kubernetes import client,config
 
 app=Flask(__name__)
+
+@app.route("/deployment",methods=["POST"])
+def deployment():
+    data = request.get_json()
+
+
+
+    if os.getenv('KUBERNETES_SERVICE_HOST'):
+        config.load_incluster_config()
+    else:
+        config.load_kube_config()
+    kube_client = client.ApiClient()
+
+    deploy = client.V1Deployment()
+
+    deploy.api_version = "apps/v1"
+    deploy.kind = "Deployment"
+    deploy.metdata = client.V1ObjectMeta(name=metadata_name)
+
+
+    spec = client.V1DeploymentSpec()
+    spec.replicas = replicas 
+
+
 
 
 if __name__ == "__main__":
